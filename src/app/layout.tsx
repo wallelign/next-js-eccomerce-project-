c8dab14ba35f28c045a885d4389d18c23c7bf09b
wallelign <walleworkieas@gmail.com>
2024-06-25 17:56:26 +0430

    shad cn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"],variable: "--font-sans", });
 
 export const metadata: Metadata = {
   title: "test App",
+
   description: "Tets react framework (next js)",
 };
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            inter.variable
+          )}> {children}
+        </body>
     </html>
   );
 }
